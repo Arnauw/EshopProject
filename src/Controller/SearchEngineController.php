d637294145150d81bebe9d53035be6ecc890bc9d
Arnauw <arnaud.rabel@gmail.com>
2025-08-21 16:08:29 +0200

    Search Engine almost done!

--- src/Controller/SearchEngineController.php
+++ src/Controller/SearchEngineController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,19 +11,22 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SearchEngineController extends AbstractController
 {
     #[Route('/search', name: 'app_search', methods: ['POST'])]
-    public function index(Request $request, $productRepo): Response
+    public function search(Request $request, ProductRepository $productRepo): Response
     {
 
+//        dd($request->isMethod(Request::METHOD_POST));
 
-
-        $request->isMethod(Request::METHOD_POST);
-
-
-        $search = $productRepo->searchEngine('juva');
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $word = $request->request->get('word');
+            $results = $productRepo->searchEngine($word);
+        } else {
+            $word = '';
+            $results = [];
+        }
 
         return $this->render('search_engine/index.html.twig', [
-//            'products' => $results,
-//            'word' => $word,
+            'products' => $results,
+            'word' => $word,
         ]);
     }
 }
